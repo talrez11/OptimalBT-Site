59f7460341b2c78941e10ddc5ce2c6c979f318b1
Eyal Gershon <eyal@way2cu.com>
2015-07-08 11:05:59 +0300

    Added thank you page.

--- site/config.php
+++ site/config.php
@@ -17,7 +17,7 @@ define('DEBUG', 1);
 // define('SQL_DEBUG', 1);
 
 // site language configuration
-$available_languages = array('he');
+$available_languages = array('he','en');
 $default_language = 'he';
 
 // default session options
